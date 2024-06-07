b819a51a91a313437c2bb217a63f939e98453c97
karenCLima <lima.kcls@gmail.com>
2024-06-07 14:42:39 -0300

    atualizando repositorio

--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-for(let elemento of vetor){
-//     elemento = parseInt(prompt('Digite um elemento para o vetor: '));
-// }
-
-// let identificador = parseInt(prompt('Escolha uma operação:\n1-soma dos elementos\n2-produto dos elemento
